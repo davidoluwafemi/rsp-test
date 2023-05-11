c4b810fe9f26861087e2f7a704966bef2f187fcb
David Oluwafemi <59213680+davidoluwafemi@users.noreply.github.com>
2023-05-11 14:07:36 +0100

    new

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set GPIO numbering mode and configure GPIO pins
-GPIO.setmode(GPIO.BCM)
-STEP_PIN = 17
-DIR_PIN = 27
-SERVO_PIN = 18
+GPIO.setmode(GPIO.BOARD)
+servo_pin = 11  # Use the GPIO pin you connected the servo's signal (SIG) pin to
 
-GPIO.setup(STEP_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(servo_pin, GPIO.OUT)
+servo = GPIO.PWM(servo_pin, 50)  # 50 Hz frequency
+servo.start(0)
 
-# Define servo control functions
 def set_angle(angle):
     duty = angle / 18 + 2
-    GPIO.output(SERVO_PIN, True)
-    p = GPIO.PWM(SERVO_PIN, 50)
-    p.start(0)
-    p.ChangeDutyCycle(duty)
-    time.sleep(0.5)
-    GPIO.output(SERVO_PIN, False)
-    p.stop()
+    GPIO.output(servo_pin, True)
+    servo.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(servo_pin, False)
+    servo.ChangeDutyCycle(0)
 
-# Define motor control functions
-def move_motor(steps, direction):
-    GPIO.output(DIR_PIN, direction)
-    for _ in range(steps):
-        GPIO.output(STEP_PIN, GPIO.HIGH)
-        time.sleep(0.001)  # Adjust delay as per your motor requirements
-        GPIO.output(STEP_PIN, GPIO.LOW)
-        time.sleep(0.001)  # Adjust delay as per your motor requirements
 
-# Usage examples
-move_motor(200, GPIO.HIGH)  # Move motor 200 steps in one direction
-set_angle(90)  # Set servo angle to 90 degrees
+set_angle(90)  # Move servo to 90 degrees
 
-# Clean up GPIO pins
+
+servo.stop()
 GPIO.cleanup()
